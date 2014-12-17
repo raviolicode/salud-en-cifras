d77ed80d124458ad6860a8974b025fe8d8df43b6
Lucia Escanellas <raviolicode@gmail.com>
2014-12-17 14:52:08 -0200

    Listing Example

--- scripts/controllers/listing.js
+++ scripts/controllers/listing.js
@@ -4,7 +4,7 @@ angular.module('saludEnCifrasApp')
   .controller('ListingController', function ($scope, $http) {
     $http.get('data/listing.json')
       .then(function(res) {
-        $scope.providers = res.data;
+        $scope.providers = res.data['providers'];
       });
   });
 
